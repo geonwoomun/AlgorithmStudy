42c0aa2303c46a6ed81a14885522f6d98fe04470
geonwoomun <ansejrrhkd@naver.com>
2019-11-22 20:59:58 +0900

    하샤드 수  개선

--- programmers/level1/pro12947.py
+++ programmers/level1/pro12947.py
@@ -1,14 +1,11 @@
-# 하샤드 수
-
+# 프로그래머스 하샤드 수
+# 좀더 개선해서 해봄
 def solution(x):
     answer = False
-
     temp = str(x)
-    temp = list(temp)
-    
     temp2 = 0
-    for value in temp:
-        temp2 += int(value)
+    for i in range(len(temp)):
+        temp2 += int(temp[i])
     if(x % temp2 == 0):
         answer = True
 
