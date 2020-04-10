25f98cf55af2c93ace1196559e536b319560b86a
geonwoomun <ansejrrhkd@naver.com>
2020-04-10 13:10:25 +0900

    윈터코딩 멀쩡한 사각형

---
+++ programmers/level2/pro62048.py
@@ -0,0 +1,12 @@
+# 프로그래머스 윈터코딩 멀쩡한 사각형
+
+def getGcd(w, h):
+    if (h == 0):
+        return w
+    return getGcd(h, w % h)
+
+def solution(w,h):
+    if (w > h):
+        w, h = h, w
+    
+    return w*h - (h + w - getGcd(w,h))
