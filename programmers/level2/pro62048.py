a3b81b294d7b0d85ce3dc0dedbbccf02bd3cb09e
geonwoomun <ansejrrhkd@naver.com>
2020-04-10 13:13:15 +0900

    math.gcd가 있었던거임...!

--- programmers/level2/pro62048.py
+++ programmers/level2/pro62048.py
@@ -1,12 +1,13 @@
 # 프로그래머스 윈터코딩 멀쩡한 사각형
 
-def getGcd(w, h):
-    if (h == 0):
-        return w
-    return getGcd(h, w % h)
+# def getGcd(w, h):
+#     if (h == 0):
+#         return w
+#     return getGcd(h, w % h)
 
+import math
 def solution(w,h):
     if (w > h):
         w, h = h, w
     
-    return w*h - (h + w - getGcd(w,h))
+    return w*h - (h + w - math.gcd(w,h))
