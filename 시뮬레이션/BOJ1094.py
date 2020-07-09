7856be1c75f88e7cc76687dc4601bc6ebdde2620
geonwoomun <38036598+geonwoomun@users.noreply.github.com>
2020-07-10 01:26:27 +0900

    알고리즘 풀기..!

--- 시뮬레이션/BOJ1094.py
+++ 시뮬레이션/BOJ1094.py
@@ -1,20 +1,35 @@
 # BOJ 1094 막대기
 
 from sys import stdin
+
 input = stdin.readline
 
 X = int(input())
 
 S = 64
 count = 0
-dist = 0
-while 1:
-    if(dist + S > X):
-        S = S//2
-    else :
-        dist += S
-        count +=1
-        if(dist == X):
-            break
 
-print(count)
+while 1:
+    if S == X:
+        count += 1
+        print(count)
+        break
+    if S < X:
+        X -= S
+        count += 1
+    S = S // 2
+
+
+# S = 64
+# count = 0
+# dist = 0
+# while 1:
+#     if(dist + S > X):
+#         S = S//2
+#     else :
+#         dist += S
+#         count +=1
+#         if(dist == X):
+#             break
+
+# print(count)
