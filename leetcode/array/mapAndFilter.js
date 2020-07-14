61326ac6f50e54bd8699c860014b937e2ea56e56
geonwoomun <38036598+geonwoomun@users.noreply.github.com>
2020-07-14 20:59:51 +0900

    filter 함수 만들어 보기

--- leetcode/array/mapAndFilter.js
+++ leetcode/array/mapAndFilter.js
@@ -6,5 +6,13 @@ function map(arr, func) {
   return result;
 }
 
+function filter(arr, func) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    func(arr[i], i) && result.push(arr[i]);
+  }
+  return result;
+}
 const arr = [1, 2, 3, 4, 5];
 console.log(map(arr, (value) => value + 2));
+console.log(filter(arr, (value) => value > 2));
